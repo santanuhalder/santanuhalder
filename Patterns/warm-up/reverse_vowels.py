78c4dbbad97b661d14376f3d1a2960edf3413f99
santanuhalder <santanu.halder@hotmail.com>
2023-12-28 19:40:44 -0800

    Initial commit

---
+++ Patterns/warm-up/reverse_vowels.py
@@ -0,0 +1,24 @@
+def reverse_vowels(txt):
+	vowel = 'aeiouAEIOU'
+	vowel_set = set(vowel)
+	low, high = 0, len(txt) - 1
+	input = list(txt)
+
+	while high > low:
+		while low < high and input[low] not in vowel_set:
+			low += 1
+		while low < high and input[high] not in vowel_set:
+			high -= 1
+		input[low], input[high] = input[high], input[low]
+		low += 1
+		high -= 1
+	return ''.join(input)
+
+
+str1 = "hello"
+str2 = "AEIOU"
+str3 = "DesignGUrus"
+
+print(reverse_vowels(str1))
+print(reverse_vowels(str2))
+print(reverse_vowels(str3))
